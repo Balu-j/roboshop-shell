09e00c8c3152f2a5364a38484ce696c7eb933a7d
Balaji <balajijanjanamm@gmail.com>
2025-03-09 01:28:13 +0530

    mongo

--- mongo.sh
+++ mongo.sh
@@ -1,8 +1,9 @@
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 
-sed -i -e 's|127.0.0.1|0.0.0.0|'
+# shellcheck disable=SC1073
+sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongo.conf
 
 
 systemctl enable mongod
-systemctl start mongod
+systemctl restart mongod
